f737b670f61f97d70ca2c88d526ab845198bf9ac
Francis Santiago <francis1505@gmail.com>
2024-11-11 07:28:19 -0300

    :sparkles: Utilizando o Formulário(formBuilder)

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormsComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup = this.formBuilder.group({
+    fistName: [''],
+    lastName: [''],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public submitForm() {
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.value.firstName);
+    console.log(this.registerForm.value.lastName);
   }
 
 }
